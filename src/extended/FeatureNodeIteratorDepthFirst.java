3c491711231434dee64cf00619e357e78e5a2546
Sascha Steinbiss <steinbiss@zbh.uni-hamburg.de>
2010-02-12 11:30:56 +0100

    reformat source in FeatureNodeIteratorDepthFirst.java

--- src/extended/FeatureNodeIteratorDepthFirst.java
+++ src/extended/FeatureNodeIteratorDepthFirst.java
@@ -3,9 +3,9 @@ package extended;
 import gtnative.*;
 
 public class FeatureNodeIteratorDepthFirst extends FeatureNodeIterator {
-    public FeatureNodeIteratorDepthFirst(FeatureNode node) {
-	synchronized (this) {
-	    feat_ptr = GT.INSTANCE.gt_feature_node_iterator_new(node.to_ptr());
-	}
+  public FeatureNodeIteratorDepthFirst(FeatureNode node) {
+    synchronized (this) {
+      feat_ptr = GT.INSTANCE.gt_feature_node_iterator_new(node.to_ptr());
     }
+  }
 }
